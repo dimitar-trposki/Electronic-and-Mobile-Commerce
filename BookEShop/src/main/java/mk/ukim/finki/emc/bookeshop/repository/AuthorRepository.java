bfa67b83351c8749ddbeb34d1b6be9b8228b80fd
dimitar-trposki <dimitar.curry30@gmail.com>
2025-05-12 22:22:03 +0200

    BookEShop removed

--- BookEShop/src/main/java/mk/ukim/finki/emc/bookeshop/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.emc.bookeshop.repository;
-
-import mk.ukim.finki.emc.bookeshop.model.domain.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
